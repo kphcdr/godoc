462c191a76ce2dd473905a168b0462cabf347033
kphcdr <kphcdr@163.com>
2018-05-15 11:48:08 +0800

    发布脚本

---
+++ delay.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+echo "拉取最新代码中。。。"
+
+git pull
+
+echo "打包程序。。。"
+
+go build
+
+echo "kill 应用。。。"
+
+ps -ef |grep "showdoc" | grep -v 'grep' | awk '{print $2}' | xargs kill -9
+
+echo "启动应用。。。"
+
+nohup /root/go/src/showdoc &
+
